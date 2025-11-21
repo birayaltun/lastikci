5d04278fdc6d991e2f7aad15ae9948d6783022db
alicorak0 <ali_123-fb@hotmail.com>
2025-11-21 23:36:36 +0300

    Modal Ekran Şablonu Eklendi

--- src/app/component/KategoriTurleri/fastfood-component/fastfood-component.ts
+++ src/app/component/KategoriTurleri/fastfood-component/fastfood-component.ts
@@ -11,8 +11,45 @@ import { CommonModule } from '@angular/common';
 })
 export class FastfoodComponent implements AfterViewInit {
 
+    selectedProduct: any = null;
+
+  products = [
+    {
+      title: 'Gurme Burger',
+      description: 'Bol çikolatalı, yumuşak ve nemli kek...',
+      price: '₺245,00',
+      tooltip: '🍔 Gurme Burger özel soslu, 220 gr etli, el yapımı hamburgerdir.',
+      image:"/slide1.jpg"
+    },
+    {
+      title: 'Margarita Pizza',
+      description: 'Mini kek, renkli kremayla süslenmiş...',
+      price: '₺125,00',
+      tooltip: '🍕 Margarita Pizza, ince hamurlu ve taze malzemelidir.',
+      image:"/slide2.jpg"
+
+    },
+    // diğer ürünler...
+  ];
+
+openModal(product: any) {
+    this.selectedProduct = product;
+  }
+
+  closeModal() {
+    this.selectedProduct = null;
+  }
+
+
   ngAfterViewInit() {
     const cards = document.querySelectorAll('.product-card');
     cards.forEach(card => card.classList.add('show')); // Tüm kartlara aynı anda uygula
+
+    
+
+
+
+
+
   }
 }
