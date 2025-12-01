aa6540c691edee47e022b593c456b763d4495f83
alicorak0 <ali_123-fb@hotmail.com>
2025-12-01 23:57:06 +0300

    1 Aralık 1

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,10 +7,11 @@ import { WheelsComponent } from './component/wheels-component/wheels-component';
 import { RoadassistanceComponent } from './component/roadassistance-component/roadassistance-component';
 import { ContactComponent } from './component/contact-component/contact-component';
 import { MainmenuComponent } from './component/mainmenu-component/mainmenu-component';
+import { FooterComponent } from './component/footer-component/footer-component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent,WheelsComponent,RouterOutlet,RouterModule,FontAwesomeModule,RoadassistanceComponent,ContactComponent,MainmenuComponent],
+  imports: [HeaderComponent,WheelsComponent,RouterOutlet,RouterModule,FontAwesomeModule,RoadassistanceComponent,ContactComponent,MainmenuComponent,FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
