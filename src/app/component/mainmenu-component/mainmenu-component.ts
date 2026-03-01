7dd870993027a22f566af58f3580dbfc022a7b96
birayaltun <149802729+birayaltun@users.noreply.github.com>
2026-03-01 11:45:24 +0300

    1 Mart 11:45

--- src/app/component/mainmenu-component/mainmenu-component.ts
+++ src/app/component/mainmenu-component/mainmenu-component.ts
@@ -1,4 +1,10 @@
-import { AfterViewInit, Component, ViewEncapsulation } from '@angular/core';
+import {
+  AfterViewInit, Component, ViewEncapsulation
+} from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ViewChild } from '@angular/core';
+import { ElementRef } from '@angular/core';
 declare var initSlider: any; // script.js içindeki fonksiyon
 
 @Component({
@@ -9,11 +15,40 @@ declare var initSlider: any; // script.js içindeki fonksiyon
 })
 export class MainmenuComponent implements AfterViewInit {
 
+
+  @ViewChild('bgVideo')
+  bgVideo!: ElementRef<HTMLVideoElement>;
+
+
   ngAfterViewInit() {
+
 
     if (initSlider) {
       initSlider();  // script.js içindeki fonksiyonu çağırıyoruz
     }
+
+    const vid = this.bgVideo.nativeElement;
+
+    vid.muted = true;
+
+    requestAnimationFrame(() => {
+      requestAnimationFrame(() => {
+        this.bgVideo.nativeElement.play().catch(() => { });
+      });
+    });
+
+    const tryPlay = () => {
+      vid.play().catch(() => {
+        setTimeout(() => vid.play(), 300);
+      });
+    };
+
+    if (vid.readyState >= 2) {
+      tryPlay();
+    } else {
+      vid.onloadeddata = tryPlay;
+    }
+
   }
 }
 
